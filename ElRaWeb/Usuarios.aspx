5e3239a6eba105e16541c23a68b9ee80611ccf6f
afranchi89 <afranchi89@hotmail.com>
2015-10-27 22:02:31 -0300

    actualizacion 20151027 2

---
+++ ElRaWeb/Usuarios.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Usuarios.aspx.cs" Inherits="Usuarios" %>
+
+<asp:Content ID="HeaderContentLogin" ContentPlaceHolderID="cphHeader" runat="server">
+    
+</asp:Content>
+<asp:Content ID="BodyContentLogin" ContentPlaceHolderID="cphBody" Runat="Server">
+    <div class="container-fluid">
+        <div class="form-inline " id="formUsuarios" runat="server" >
+            <div class="col-sm-4 col-sm-offset-2 col-md-4 col-md-offset-4">
+                <div class="panel panel-primary">
+                     <div class="panel-heading text-center">
+                        <h4 class="h4">Login</h4>
+                    </div>
+                      <div class="panel-body">                    
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sp1"">E-Mail</span>
+                                    <asp:TextBox ID="tbMail" class="form-control" aria-describedby="sp2" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                           
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sp1"">Nombre</span>
+                                    <asp:TextBox ID="tbNombre" class="form-control" aria-describedby="sp2" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sp1"">Apellido</span>
+                                    <asp:TextBox ID="tbApellido" class="form-control" aria-describedby="sp2" runat="server"></asp:TextBox>
+                                </div>
+                            </div>
+                           <hr />                   
+                    
+                        <div class="form-group text-center">
+                            <asp:Button ID="btnBuscar" class="btn btn-primary" runat="server" onclick="btnBuscar_Click" Text="Buscar"/>
+                        </div>
+                        <div class="form-group text-center">
+                            <asp:Button ID="btnNuevo" class="btn btn-primary" runat="server" onclick="btnNuevo_Click" Text="Nuevo Usuario"/>
+                        </div>
+                    </div>
+                    <div class ="panel-footer">
+                        <asp:GridView id="dgResultados" runat="server" AutoGenerateColumns="False" OnRowCreated="dgResultados_RowCreated" OnRowDeleting="dgResultados_RowDeleting" OnRowEditing="dgResultados_RowEditing" DataKeyNames="Legajo">
+					        <Columns>
+                                <asp:CommandField EditText="Editar" ShowEditButton="True" />
+                                <asp:CommandField DeleteText="Borrar" ShowDeleteButton="True" />
+						        <asp:BoundField DataField="Correo" HeaderText="Legajo"></asp:BoundField>
+						        <asp:BoundField DataField="Apellido" HeaderText="Apellido"></asp:BoundField>
+						        <asp:BoundField DataField="Nombre" HeaderText="Nombre"></asp:BoundField>
+					        </Columns>
+				        </asp:GridView>
+                   </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
+    
+
---
+++ ElRaWeb/Usuarios.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ElRaBusiness;
+using ElRaEntity;
+using ElRaComun;
+using ElRaWebUtil;
+
+public partial class Usuarios : System.Web.UI.Page
+{
+    private UsuarioBO boUsuario = new UsuarioBO();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    // Constantes de columnas de la grilla.
+    private const int DGCOLUMN_BORRAR = 1;
+    
+    /// <summary>
+    /// Busca los empleados y carga la grilla.
+    /// </summary>
+    private void BuscarEmpleados()
+    {
+        try
+        {
+            dgResultados.DataSource = boUsuario.Buscar(tbMail.Text, tbNombre.Text, tbApellido.Text);
+            dgResultados.DataBind();
+        }
+        catch (Exception ex)
+        {
+            throw new ExcepcionBO("Error", ex);
+        }
+    }
+
+     /// <summary>
+    /// Se ejecuta al presionar el botón buscar.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnBuscar_Click(object sender, System.EventArgs e)
+    {
+        BuscarEmpleados();
+    }
+
+    /// <summary>
+    /// Se ejecuta al presionar el botón nuevo.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnNuevo_Click(object sender, System.EventArgs e)
+    {
+        // Se transfiere la ejecución a la página de carga y modificación de empleado.
+        Server.Transfer("Registro.aspx");
+    }
+
+    /// <summary>
+    /// Si se presiona el link "Editar" de la grilla.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="e">Referencia a la fila en la cual se presionó.</param>
+    protected void dgResultados_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        try
+        {
+            // Se busca el empleado por el legajo y se envía a la página de edición.
+
+            Context.Items.Add("Usuario", boUsuario.Buscar(dgResultados.DataKeys[e.NewEditIndex].Value.ToString(), "", ""));
+            // Se transfiere la ejecución a la página de carga y modificación de empleado.
+            Server.Transfer("Registro.aspx");
+        }
+        catch (Exception ex)
+        {
+            throw new ExcepcionBO("Error", ex);
+        }
+    }
+
+    /// <summary>
+    /// Si se presiona el link "Borrar" de la grilla.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="e">Referencia a la fila en la cual se presionó.</param>
+    protected void dgResultados_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        try
+        {
+            // Se busca el empleado por el legajo y se lo borra.
+            //boUsuario.Eliminar(dgResultados.DataKeys[e.RowIndex].Value.ToString());
+            // Se recarga la grilla.
+            BuscarEmpleados();
+        }
+        catch (Exception ex)
+        {
+            throw new ExcepcionBO("Error", ex);
+        }
+    }
+
+    /// <summary>
+    /// Al crearse un item (una fila de la grilla) se le asigna al link "Borrar"
+    /// una función que pide la confirmación antes de eliminar al empleado. 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e">Referencia a la fila creada.</param>
+    protected void dgResultados_RowCreated(object sender, GridViewRowEventArgs e)
+    {
+        // onclick: al hacer click con el mouse sobre el link
+        // se ejecuta "return confirm('¿Está seguro que desea borrar al contacto?');"
+        e.Row.Cells[DGCOLUMN_BORRAR].Attributes.Add("onclick", "javascript: return confirm('¿Está seguro que desea borrar al empleado?');");
+    }
+
+
+
+}
